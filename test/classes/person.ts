5eb8ccd9cff1db38f24c210c10506aeea76edb52
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2023-05-26 22:23:47 -0300

    Implements relatedTo by add foreing keys

--- test/classes/person.ts
+++ test/classes/person.ts
@@ -1,17 +1,20 @@
 import "reflect-metadata";
 import { SqlColumn } from "../../src/core/decorators/column/sqlColumn.decorator";
 import { SqlModel } from "../../src/core/interfaces/database/misc/sqlModel.interface";
+import { RelatedTo } from "../../src/core/decorators/relatedTo/relatedTo.decorator";
 import { SqlKey } from "../../src/core/types/dataTypes/primaryKey.types";
+import { Materias } from "./materias";
 
 export class Person extends SqlModel {
   @SqlColumn({ type: "INT", pk: true, autoIncrement: true })
   id_person: SqlKey = 0;
-
+  @SqlColumn({ type: "INT" })
+  id_materia: number = 0;
+  @RelatedTo({ model: new Materias(), key: "id_materia" })
+  fk_materia: number = 0;
 
   @SqlColumn({ type: "TEXT" })
   apellido: string = "";
-  @SqlColumn({ type: "TEXT" })
-  apellido2: string = "";
   @SqlColumn({ type: "TEXT" })
   account: string = "";
 
