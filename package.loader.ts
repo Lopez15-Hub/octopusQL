4d9b81329e0f43e1036c6099784e05b04259bfca
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2023-05-29 18:13:47 -0300

    V1.0.24 fixed errors

--- package.loader.ts
+++ package.loader.ts
@@ -2,6 +2,6 @@ import { Package } from "./src/core/interfaces/app/package.interface";
 
 export const packageMetadata: Package = {
   name: "octopusql",
-  version: "1.0.23",
+  version: "1.0.24",
   description: "Use your Relationals DB Easily.",
 };
