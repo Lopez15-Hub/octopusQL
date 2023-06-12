ddfd7374f19e9e4afb67994bebcdb351b314fb4f
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2023-06-12 11:30:35 -0300

    V1.0.34

--- package.loader.ts
+++ package.loader.ts
@@ -2,6 +2,6 @@ import { Package } from "./src/core/interfaces/app/package.interface";
 
 export const packageMetadata: Package = {
   name: "octopusql",
-  version: "1.0.33",
+  version: "1.0.34",
   description: "Use your Relationals DB Easily.",
 };
